5abd6564997f377ac0afeaf41e550e97d5746c83
Your Name <haoma7@gmail.com>
2022-10-11 11:59:44 -0700

    the second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-// import {User, SecondUser} from "./User";
-import Data from './Data';
 import Form from "./Form";
 function App() {
   const name = "Matt";
@@ -10,20 +8,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
         <Data />
-        {/* <User name={name} age = {age}/>
-        <SecondUser /> */}
+
         <Form />
       </header>
     </div>
